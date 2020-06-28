2db6c8d60a268e1600f1cb877c62476aa4f043ea
Brian Racca <bracca@tekgenesis.com>
2020-06-28 17:58:08 -0300

    searcher functionality

--- lib/services/category_repository.dart
+++ lib/services/category_repository.dart
@@ -2,6 +2,7 @@ import 'package:qapaq_b2b/models/category.dart';
 
 abstract class CategoryRepository {
   List<CategoryModel> list();
+  List<CategoryModel> listByName(String pattern);
 
   CategoryModel findByName(String name);
 }
