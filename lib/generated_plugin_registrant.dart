12f16d7b87ed8ab311a63de4e3c4c282ca38b9af
Mauricio Valli <mauricio.valli@bpmx.com.ar>
2020-07-29 15:45:49 -0300

    Merge changes

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -8,15 +8,15 @@ import 'dart:ui';
 import 'package:firebase_analytics_web/firebase_analytics_web.dart';
 import 'package:firebase_auth_web/firebase_auth_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
-import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:google_sign_in_web/google_sign_in_web.dart';
+
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 // ignore: public_member_api_docs
 void registerPlugins(PluginRegistry registry) {
+  FirebaseAnalyticsWeb.registerWith(registry.registrarFor(FirebaseAnalyticsWeb));
   FirebaseAuthWeb.registerWith(registry.registrarFor(FirebaseAuthWeb));
   FirebaseCoreWeb.registerWith(registry.registrarFor(FirebaseCoreWeb));
-  FirebaseAnalyticsWeb.registerWith(
-      registry.registrarFor(FirebaseAnalyticsWeb));
   GoogleSignInPlugin.registerWith(registry.registrarFor(GoogleSignInPlugin));
   registry.registerMessageHandler();
 }
